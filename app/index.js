618d65dc247ed547cecefdb41ec8153f55668754
iamlydial <lydia.lavecchia@gmail.com>
2024-04-22 23:03:54 +0200

    feat(setup) add necessary components, constants and assets

---
+++ app/index.js
@@ -0,0 +1,46 @@
+import { View, ScrollView, SafeAreaView, Text } from "react-native-web";
+import { Stack, useRouter } from "expo-router";
+import { useState } from "react";
+
+import { COLORS, SIZES, icons, images, sizes } from "../constants";
+
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
+
+const Home = () => {
+  const router = useRouter();
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={icons.profile} dimension="100%" />
+          ),
+          headerTitle: "",
+        }}
+      />
+
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View 
+        style={{ 
+            flex: 1, 
+            padding: SIZES.medium }}>
+          <Welcome />
+          <Popularjobs />
+          <Nearbyjobs />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
